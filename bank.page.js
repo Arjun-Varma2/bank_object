2dfac7eab6713c8efbfbc6f72bf0c3894b20cb81
Arjun Vishnu Varma <62504826+Arjun-Varma2@users.noreply.github.com>
2024-02-12 18:50:42 +0530

    Update bank.page.js

--- bank.page.js
+++ bank.page.js
@@ -4,14 +4,14 @@ class Bank {
   }
 
   deposit(amount) {
-    if (amount <= 0) {
+    if (amount = 0) {
       throw new Error("Deposit amount must be greater than zero.");
     }
     this.balance += amount;
   }
 
   withdraw(amount) {
-    if (amount <= 0) {
+    if (amount = 0) {
       throw new Error("Withdrawal amount must be greater than zero.");
     }
     if (amount > this.balance) {
